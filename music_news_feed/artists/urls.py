6deb49d4e81d80e7febee46e206aaebcc81fbc42
palasonicq <mvserebryakov@edu.hse.ru>
2018-12-16 18:43:13 +0300

    artists: complete search page

--- music_news_feed/artists/urls.py
+++ music_news_feed/artists/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'artists'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    #url(r'^add_user', views.add_user, name='add_user'),
+    url(r'^search_artists', views.search_artists, name='search_artists'),
 ]
 
--- music_news_feed/artists/views.py
+++ music_news_feed/artists/views.py
@@ -4,7 +4,18 @@ from __future__ import unicode_literals
 from django.conf import settings
 
 from django.shortcuts import render, render_to_response, redirect, reverse
+from spotify_client import scripts
 
 def index(r):
     return render(r, 'artists/artists.html')
+
+
+def search_artists(r):
+    form = {
+        'artist_name': r.GET.get('artist_name', '')
+    }
+
+    search_result = scripts.search_artist(r.user, form['artist_name'])
+
+    return render(r, 'artists/search_artists.html', {'form': form, 'search_result': search_result})
 
